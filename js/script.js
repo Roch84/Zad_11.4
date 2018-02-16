93f40cb80cf1f9dd7be059d1c9a4622f0eb24096
Jacek Kotlarski <kotlarski.j@gmail.com>
2018-02-16 20:30:35 +0100

    Task_11.4_update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, screen, color) {
 	this.brand = brand;
 	this.price = price;
+	this.screen = screen;
 	this.color = color;
+	
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " screen size is " + this.screen + " and the price is " + this.price + ".");
+	alert("The phone brand is " + this.brand + ", color is " + this.color + " screen size is " + this.screen + " and the price is " + this.price + ".");
 }
-var iPhone6S = new Phone("Apple", 2250, "silver"),
-	GalaxyS6 = new Phone ("Samsung", 3880, "gold"),
-	OnePlus = new Phone ("Microsoft", 4503, "black");
+var iPhone6S = new Phone("Apple", 2250, "18:9", "silver"),
+	GalaxyS6 = new Phone ("Samsung", 3880, "14:6", "gold"),
+	OnePlus = new Phone ("Microsoft", 4503, "16:8", "black");
 
 iPhone6S.printInfo();
 GalaxyS6.printInfo();
