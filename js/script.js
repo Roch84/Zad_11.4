bf8e19a561de093928c96836549b8dfff68e31da
Jacek Kotlarski <kotlarski.j@gmail.com>
2018-02-23 22:23:36 +0100

    Change js, index code after review

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ Phone.prototype.printInfo = function() {
 	alert("The phone brand is " + this.brand + ", color is " + this.color + " screen size is " + this.screen + " and the price is " + this.price + ".");
 }
 var iPhone6S = new Phone("Apple", 2250, "18:9", "silver"),
-	GalaxyS6 = new Phone ("Samsung", 3880, "14:6", "gold"),
-	OnePlus = new Phone ("Microsoft", 4503, "16:8", "black");
+	galaxyS6 = new Phone("Samsung", 3880, "14:6", "gold"),
+	onePlus = new Phone("Microsoft", 4503, "16:8", "black");
 
 iPhone6S.printInfo();
-GalaxyS6.printInfo();
-OnePlus.printInfo();
+galaxyS6.printInfo();
+onePlus.printInfo();
